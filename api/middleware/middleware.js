f612176d262138e05f145df273bb56c1cfcbec16
John Merz <jmerz826@gmail.com>
2022-01-12 21:08:13 -0500

    logger middleware working

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,11 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log(`request method: ${req.method}`)
+  console.log(`request url: ${req.url}`)
+  const timestamp = new Date(Date.now())
+  console.log(timestamp.toLocaleString())
+
+  next()
 }
 
 function validateUserId(req, res, next) {
@@ -15,3 +21,6 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,44 +1,58 @@
-const express = require('express');
+const express = require('express')
 
 // You will need `users-model.js` and `posts-model.js` both
+const User = require('./users-model')
+const Post = require('../posts/posts-model')
 // The middleware functions also need to be required
+const {
+  logger
+} = require('../middleware/middleware')
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', logger, (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
-});
+  User.get()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(err => {
+      res.status(500).json({message: err.message})
+    })
+})
 
 router.get('/:id', (req, res) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
-});
+})
 
 router.post('/', (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
-});
+})
 
 router.put('/:id', (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
-});
+})
 
 router.delete('/:id', (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
-});
+})
 
 router.get('/:id/posts', (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
-});
+})
 
 router.post('/:id/posts', (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
-});
+})
 
 // do not forget to export the router
+
+module.exports = router
